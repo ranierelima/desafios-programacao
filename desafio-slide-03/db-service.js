747e7b039c85a98a636a39340a09a61947e6ab4e
Raniere de Lima <ranieredelima@gmail.com>
2022-04-06 21:17:10 -0300

    Atualizando arquivos que simula banco de dados

--- desafio-slide-03/db-service.js
+++ desafio-slide-03/db-service.js
@@ -1,10 +1,24 @@
 function recuperarDados(){
-    return window.localStorage.getItem('contatos') || [];
+    let lista = window.localStorage.getItem('contatos');
+    if(lista) {
+        lista = JSON.parse(lista).map(item => JSON.parse(item));
+    }
+        
+    return lista || [];
 }
 
 function salvarDados(contato){
     let contatos = recuperarDados();
  
-    contatos.push(contato);
-    window.localStorage.setItem('contatos', contatos);
+    let indexContato = contatos.indexOf(c => JSON.stringify(c) == JSON.stringify(contato) );
+
+    if(indexContato == -1 ){
+        contatos.push(JSON.stringify(contato));
+    } else {
+        contatos[indexContato] = JSON.stringify(contato);
+    }
+    
+    window.localStorage.setItem('contatos', JSON.stringify(contatos));
+
+    return contatos;
 }
