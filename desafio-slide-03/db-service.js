bf0ce34400bfb4f10b33ab85b9239141a13bd54b
Raniere de Lima <ranieredelima@gmail.com>
2022-04-13 21:51:03 -0300

    Update db-service.js

--- desafio-slide-03/db-service.js
+++ desafio-slide-03/db-service.js
@@ -1,24 +1,44 @@
 function recuperarDados(){
     let lista = window.localStorage.getItem('contatos');
     if(lista) {
-        lista = JSON.parse(lista).map(item => JSON.parse(item));
-    }
-        
+        lista = JSON.parse(lista);
+    }   
     return lista || [];
 }
 
 function salvarDados(contato){
     let contatos = recuperarDados();
  
-    let indexContato = contatos.indexOf(c => JSON.stringify(c) == JSON.stringify(contato) );
+    if(!contato.id){
+        contato.id = generateUuid();
+    }
+
+    let indexContato = contatos.findIndex(function(c){
+        return c.id == contato.id
+    });
 
     if(indexContato == -1 ){
-        contatos.push(JSON.stringify(contato));
+        contatos.push(contato);
     } else {
-        contatos[indexContato] = JSON.stringify(contato);
+        contatos[indexContato] = contato;
     }
     
+    window.localStorage.setItem('contatos', JSON.stringify(contatos));
+    return contatos;
+}
+
+function removerContato(uuid){
+    let contatos = recuperarDados();
+    contatos = contatos.filter(function(c) {
+        return c.id != uuid;
+    });
     window.localStorage.setItem('contatos', JSON.stringify(contatos));
 
     return contatos;
 }
+
+function generateUuid() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
